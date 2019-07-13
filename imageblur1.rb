2984c438c6e85da44d6de07a6e374d1dcc7708e6
Barbara Sanders <barbarasanderstruth@gmail.com>
2019-07-13 15:56:15 +0000

    Add class

--- imageblur1.rb
+++ imageblur1.rb
@@ -1,7 +1,28 @@
 # imageblur1.rb
 
-image = [[0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]]
+#image = [[0,0,0,0], [0,1,0,0], [0,0,0,1], [0,0,0,0]]
 
-image.each do |s|
-  puts s.inspect
+class Image
+  def initialize(pixels)
+    @blur = pixels
+  end
+  
+  def output_image
+    @blur.each do |s|
+      puts s.join
+    end
+  end
 end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
+
+#image.each do |s|
+  #puts s.inspect
+#end
